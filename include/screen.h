c2e768e02590177e781d2ac544a1faf5147cf28b
TheOnlyZac <zacharykrasnow@gmail.com>
2021-12-01 22:26:45 -0500

    started screen with BLOT struct

---
+++ include/screen.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum class BLOTS : int
+{
+	Hidden = 0x0,
+	Appearing = 0x1,
+	Visible = 0x2,
+	Disappearing = 0x3,
+	Max = 0x4
+};
+
+struct BLOT
+{
+	// todo
+	int padding[0x280];
+}
+
+void SetBlotBlots(BLOT pblot, BLOTS blots);
