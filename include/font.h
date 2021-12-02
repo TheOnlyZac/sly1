658c22ccdde5fdc1b066fbdb48d1ab7a258439d1
TheOnlyZac <zacharykrasnow@gmail.com>
2021-12-01 23:25:52 -0500

    implemented CTextBox methods

--- include/font.h
+++ include/font.h
@@ -1,17 +1,44 @@
 #pragma once
 
+enum class JH : int
+{
+	JH_Left = 0x0,
+	JH_Center = 0x1,
+	JH_Right = 0x2,
+	JH_Max = 0x3
+};
+
+enum class JV : int
+{
+	JH_Top = 0x0,
+	JH_Center = 0x1,
+	JH_Bottom = 0x2,
+	JH_Max = 0x3
+};
+
+struct RGBA {
+    unsigned char ab[1];
+    char bGreen;
+    char bBlue;
+    char bAlpha;
+    char u1bRed;
+};
+
 class CTextBox
 {
-	float m_x;
-	float m_y;
-	float m_dx;
-	float m_dy;
-	/* RGBA m_rgba; todo */
-	/* JH m_jh; todo */
-	char padding[3];
-	/* JV m_jv; todo */
+private:
+	float m_x; // x pos
+	float m_y; // y pos
+	float m_dx; // x size
+	float m_dy; // y size
+	RGBA m_rgba; // text color
+	JH m_jh; // horizontal justification
+	JV m_jv; // vertical justification
 
 public:
 	void SetPos(float x, float y);
-
+	void SetSize(float dx, float dy);
+	void SetTextColor(RGBA rgba);
+	void SetHorizontalJust(JH jh);
+	void SetVerticalJust(JV jv);
 };
--- src/font.cpp
+++ src/font.cpp
@@ -5,3 +5,24 @@ void CTextBox::SetPos(float x, float y)
 	this->m_x = x;
 	this->m_y = y;
 }
+
+void CTextBox::SetSize(float dx, float dy)
+{
+	this->m_dx = dx;
+	this->m_dy = dy;
+}
+
+void CTextBox::SetTextColor(RGBA rgba)
+{
+	this->m_rgba = rgba;
+}
+
+void CTextBox::SetHorizontalJust(JH jh)
+{
+	this->m_jh = jh;
+}
+
+void CTextBox::SetVerticalJust(JV jv)
+{
+	this->m_jv = jv;
+}
