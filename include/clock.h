aafa3a0f7c31b596287e0c1412c8f624ac5dfce0
TheOnlyZac <zacharykrasnow@gmail.com>
2021-12-03 19:16:43 -0500

    fixed TickNow prototype

--- include/clock.h
+++ include/clock.h
@@ -2,10 +2,10 @@
 
 struct CLOCK
 {
-	bool fEnabled;
+	bool fEnabled; // clock enabled flag
 	float t;
 	float dt; // time since last frame
-	float dtPrev;
+	float dtPrev; // dt from previous frame
 	float tReal;
 	float dtReal;
 	long tickFrame; // todo: type shuld be TICK
@@ -18,8 +18,8 @@ void SetClockEnabled(CLOCK* pclock, bool fEnabled);
 void ResetClock(CLOCK* pclock, float t);
 void MarkClockTick(CLOCK* pclock);
 void SetClockRate(float rt);
+unsigned long TickNow(); // todo: return type should be TICK
 
 //void MarkClockTickRealOnly(CLOCK* pclock);
 //void SetClockDebugRate(int nParam);
 //void StartupClock();
-//TICK TickNow();
