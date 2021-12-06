fa307e58153f0d2734f5d21c023266c973882738
theclub654 <61169936+theclub654@users.noreply.github.com>
2021-12-06 18:36:40 -0500

    took some uneccesary parameters out

--- src/WalCatalog.cpp
+++ src/WalCatalog.cpp
@@ -1,16 +1,16 @@
 #include <WalCatalog.h>
 
-void CWalCatalog::Init(CWalCatalog *wac, CFileLocation* wac_file_desc0, CFileLocation* wal_file_desc1)
+void CWalCatalog::Init(FileLocation* wac_file_desc0, FileLocation* wal_file_desc1)
 {
-    wac->wac_sector_offset = wac_file_desc0->sector_offset;
-    wac->wac_size = wac_file_desc0->size;
-    wac->wal_sector_offset = wal_file_desc1->sector_offset;
-    wac->wal_size = wal_file_desc1->size;
-    Reload(wac);
+    wac_sector_offset = wac_file_desc0->sector_offset;
+    wac_size = wac_file_desc0->size;
+    wal_sector_offset = wal_file_desc1->sector_offset;
+    wal_size = wal_file_desc1->size;
+    Reload();
     return;
 }
 
-void CWalCatalog::Reload(CWalCatalog* wac)
+void CWalCatalog::Reload()
 {
 
 }
