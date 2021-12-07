a3a1f21d5074f67e30e530eccf355a6aa9ad1902
TheOnlyZac <zacharykrasnow@gmail.com>
2021-12-07 02:41:41 -0500

    redefined CProg as a class in prog.h

--- include/bis.h
+++ include/bis.h
@@ -1,3 +1,5 @@
+#include <prog.h>
+
 #include <iostream>
 
 typedef unsigned char byte;
@@ -5,30 +7,6 @@ typedef unsigned char byte;
 class CBinaryInputStream
 {
     public:
-
-    struct CProg
-    {
-        int field_0x0;
-        int field_0x4;
-        int field_0x8;
-        int field_0xc;
-        int field_0x10;
-        byte field_0x14;
-        byte field_0x15;
-        byte field_0x16;
-        byte field_0x17;
-        byte field_0x18;
-        byte field_0x19;
-        byte field_0x1a;
-        byte field_0x1b;
-        byte field_0x1c;
-        byte field_0x1d;
-        byte field_0x1e;
-        byte field_0x1f;
-        byte field_0x20;
-        byte field_0x21;
-        byte field_0x22;
-    };
 
     uint32_t field_0x0;
     int field_0x4;
