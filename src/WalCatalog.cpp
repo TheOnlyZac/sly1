02a69a595f0b3b71b20e45df97d0a19b70fd4e4d
theclub654 <61169936+theclub654@users.noreply.github.com>
2021-12-06 19:59:52 -0500

    Update WalCatalog.cpp

--- src/WalCatalog.cpp
+++ src/WalCatalog.cpp
@@ -2,10 +2,10 @@
 
 void CWalCatalog::Init(FileLocation* wac_file_desc0, FileLocation* wal_file_desc1)
 {
-    wac_sector_offset = wac_file_desc0->sector_offset;
-    wac_size = wac_file_desc0->size;
-    wal_sector_offset = wal_file_desc1->sector_offset;
-    wal_size = wal_file_desc1->size;
+    this->wac_sector_offset = wac_file_desc0->sector_offset;
+    this->wac_size = wac_file_desc0->size;
+    this->wal_sector_offset = wal_file_desc1->sector_offset;
+    this->wal_size = wal_file_desc1->size;
     Reload();
     return;
 }
