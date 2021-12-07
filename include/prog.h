7a64cb0df6fa06ec49b8815df310ef9dbc5ac17d
TheOnlyZac <zacharykrasnow@gmail.com>
2021-12-07 02:56:46 -0500

    fix prog.h

---
+++ include/prog.h
@@ -0,0 +1,28 @@
+#pragma once
+
+typedef unsigned char byte;
+
+class CProg
+{
+public:
+	int field_0x0;
+	int field_0x4;
+	int field_0x8;
+	int field_0xc;
+	int field_0x10;
+	byte field_0x14;
+	byte field_0x15;
+	byte field_0x16;
+	byte field_0x17;
+	byte field_0x18;
+	byte field_0x19;
+	byte field_0x1a;
+	byte field_0x1b;
+	byte field_0x1c;
+	byte field_0x1d;
+	byte field_0x1e;
+	byte field_0x1f;
+	byte field_0x20;
+	byte field_0x21;
+	byte field_0x22;
+};
