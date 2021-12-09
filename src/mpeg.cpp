9af7c7512bf48679c744a77c507f040bfeb5fae5
theclub654 <61169936+theclub654@users.noreply.github.com>
2021-12-08 19:07:03 -0500

    fixed

--- src/mpeg.cpp
+++ src/mpeg.cpp
@@ -1,21 +1,21 @@
-#include <mpeg.h>
+#include <MPeg.h>
 
-void CMpeg::Execute(CutsceneManager* cutscene_manager)
+void CMpeg::Execute()
 {
     OID* cutscene;
     OID* second_cutscene;
 
     /* Store values from cutscene mgr */
-    second_cutscene = cutscene_manager->second_cutscene;
-    cutscene = cutscene_manager->name;
+    second_cutscene = this->second_cutscene;
+    cutscene = this->name;
     /* Reset cutscene mgr values to Null */
-    cutscene_manager->name = (OID*)0x0;
-    cutscene_manager->second_cutscene = (OID*)0x0;
-    play_cutscene(cutscene_manager, cutscene);
+    this->name = (OID*)0x0;
+    this->second_cutscene = (OID*)0x0;
+    play_cutscene ? (this, cutscene);
     /* If a second cutscene is speficied, play it immediately
        after the first one */
     if (second_cutscene != (OID*)0x0) {
-        play_cutscene(cutscene_manager, second_cutscene);
+        play_cutscene ? (this, second_cutscene);
     }
     return;
 }
