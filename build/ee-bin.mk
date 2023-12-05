743f288c2ae13e128914845b8af62fe726ebc2c0
modeco80 <lily.modeco80@protonmail.ch>
2023-12-04 21:32:37 -0500

    build: Remove LDSCRIPT from ee-bin.mk linker invocation
    It expands to nothing, which is harmless, but it also isn't set or used.
    LDFLAGS has the -T ... linker script flag in it.

--- build/ee-bin.mk
+++ build/ee-bin.mk
@@ -1,7 +1,7 @@
 OBJS = crt0.o $(SRCS:.cpp=.o)
 
 $(NAME): $(OBJS)
-	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDSCRIPT) $(LDFLAGS)
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
 
 clean-products:
 	$(RM) -f $(OBJS) $(NAME)
