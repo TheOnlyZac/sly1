cbecf1b6e8e9f024398bb5ec120578865b734e2d
modeco80 <lily.modeco80@protonmail.ch>
2023-12-10 06:33:27 -0500

    build: Add `objonly`/`objflush` targets
    This allows tooling (or developers) to build a single object file simply by specifying the object to compile and running the "objonly" target.
    
    For example: `make COMPILEME=bis.o objonly` will compile only bis.o and also output the path to the final object file, useful for tools which want to scan for it.
    
    The "objflush" target also exists for if a clean build of the object file is required.

--- build/core.mk
+++ build/core.mk
@@ -7,6 +7,7 @@
 
 # TODO: Other configurations if possible
 debug_Valid := y
+#debugopt_Valid := y
 release_Valid := y
 
 # There's probably a slightly better detection heuristic
@@ -25,5 +26,24 @@ ifneq ($($(CONFIG)_Valid),y)
 $(error Invalid configuration)
 endif
 
+# Moved here since it doesn't really have to be specified in the target common file.
+OBJDIR := obj/$(CONFIG)
+
 include build/$(TARGET)-$(TARGETTYPE).mk
 
+# Make targets for allowing "only compile/only remove this object file" workflows
+# Useful for decompilation tooling that wants to use this.
+.PHONY: objonly objflush
+
+ifeq ($(COMPILEME),)
+# Running these targets without a object file to compile is a logic error.
+objonly:
+	$(error No object file provided, please set the COMPILEME variable to the object file you want to compile.)
+objflush:
+	$(error No object file provided)
+else
+objonly: $(OBJDIR)/ $(OBJDIR)/$(COMPILEME)
+	$(info $(OBJDIR)/$(COMPILEME))
+objflush:
+	$(RM) $(OBJDIR)/$(COMPILEME)
+endif
