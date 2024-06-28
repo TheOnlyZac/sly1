1b9ac5cf3f25630a2da6c30ffb8bfeb7dc1bc722
Zac <theonlyzac@gmail.com>
2024-06-28 08:11:44 +0000

    Decomp util functions

---
+++ build/README.txt
@@ -0,0 +1,1 @@
+These build scripts aren't used anymore, but they are kept here for reference.
---
+++ src/P2/util.c
@@ -0,0 +1,58 @@
+#include "common.h"
+#include <util.h>
+
+INCLUDE_ASM(const s32, "P2/util", RadNormalize);
+
+INCLUDE_ASM(const s32, "P2/util", GLimitAbs);
+
+INCLUDE_ASM(const s32, "P2/util", GSmooth);
+
+INCLUDE_ASM(const s32, "P2/util", GSmoothA);
+INCLUDE_ASM(const s32, "P2/util", func_001EA720);
+
+INCLUDE_ASM(const s32, "P2/util", RadSmooth);
+
+INCLUDE_ASM(const s32, "P2/util", RadSmoothA);
+
+INCLUDE_ASM(const s32, "P2/util", PosSmooth);
+
+INCLUDE_ASM(const s32, "P2/util", SmoothMatrix);
+
+INCLUDE_ASM(const s32, "P2/util", NRandInRange);
+
+INCLUDE_ASM(const s32, "P2/util", GRandInRange);
+
+INCLUDE_ASM(const s32, "P2/util", GRandGaussian);
+
+INCLUDE_ASM(const s32, "P2/util", FFloatsNear);
+
+INCLUDE_ASM(const s32, "P2/util", CSolveQuadratic);
+
+INCLUDE_ASM(const s32, "P2/util", PrescaleClq);
+
+INCLUDE_ASM(const s32, "P2/util", CalculateSinCos);
+
+INCLUDE_ASM(const s32, "P2/util", GTrunc);
+
+INCLUDE_ASM(const s32, "P2/util", GTrunc1);
+
+INCLUDE_ASM(const s32, "P2/util", GModPositive);
+
+INCLUDE_ASM(const s32, "P2/util", FitClq);
+
+int FCheckLm(struct LM* plm, float g)
+{
+	return (plm->gMin < g) && (g < plm->gMax);
+}
+
+INCLUDE_ASM(const s32, "P2/util", FCheckAlm);
+
+INCLUDE_ASM(const s32, "P2/util", GLimitLm);
+
+INCLUDE_ASM(const s32, "P2/util", SgnCompareG);
+
+void Force(void) {
+}
+
+INCLUDE_ASM(const s32, "P2/util", MinimizeRange);
+INCLUDE_ASM(const s32, "P2/util", func_001EB458);
