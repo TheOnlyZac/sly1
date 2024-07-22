ab7ef029f99303a44f283a63cb1e436de0ef458e
Zac <theonlyzac@gmail.com>
2024-07-22 02:53:45 +0000

    Split jt.c

---
+++ src/P2/jt2.c
@@ -0,0 +1,32 @@
+#include <jt.h>
+
+INCLUDE_ASM(const s32, "P2/jt2", UpdateJt__FP2JTf);
+
+INCLUDE_ASM(const s32, "P2/jt2", UpdateJtDrive__FP2JT);
+
+INCLUDE_ASM(const s32, "P2/jt2", ChooseJtPhys__FP2JT);
+
+INCLUDE_ASM(const s32, "P2/jt2", EnableJtActadj__FP2JTi);
+
+INCLUDE_ASM(const s32, "P2/jt2", SetJtJts__FP2JT3JTS4JTBS);
+
+//INCLUDE_ASM(const s32, "P2/jt2", ProfileJt__FP2JTi);
+void ProfileJt(JT* pjt, int fProfile) {
+    // Stubbed, does nothing.
+    return;
+}
+
+INCLUDE_ASM(const s32, "P2/jt2", SetJtPuppet__FP2JTP5ASEGA);
+INCLUDE_ASM(const s32, "P2/jt2", func_00177828);
+
+INCLUDE_ASM(const s32, "P2/jt2", PaloAbsorbWkr__FP3WKRiPP3ALO);
+INCLUDE_ASM(const s32, "P2/jt2", func_00177968);
+
+INCLUDE_ASM(const s32, "P2/jt2", UpdateJtEffect__FP2JT);
+
+INCLUDE_ASM(const s32, "P2/jt2", FIsJtSoundBase__FP2JT);
+
+INCLUDE_ASM(const s32, "P2/jt2", CollectJtPrize__FP2JT3PCKP3ALO);
+INCLUDE_ASM(const s32, "P2/jt2", func_00177EE0);
+
+INCLUDE_ASM(const s32, "P2/jt2", PlayJtDialog__FP2JTP6DIALOG);
