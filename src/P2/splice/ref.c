4d9893f704fee5b1280628f6cc466b3ff674f569
Zac <theonlyzac@gmail.com>
2024-08-03 05:12:41 +0000

    Split splice/ref.cpp

---
+++ src/P2/splice/ref.c
@@ -0,0 +1,51 @@
+#include "common.h"
+
+INCLUDE_ASM(const s32, "P2/splice/ref", __4CRef);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", __4CRefRC4CRef);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", _$_4CRef);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", __as__4CRefRC4CRef);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", __eq__C4CRefRC4CRef);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", CloneTo__4CRefP4CRefP6CFrame);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", Decref__C4CRef);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", Incref__C4CRef);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetTag__4CRef4TAGK);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetS32__4CRefi);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetF32__4CReff);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetBool__4CRefi);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetSymid__4CRefUi);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetBifk__4CRef4BIFK);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetPair__4CRefP5CPair);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetProc__4CRefP5CProc);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetVector__4CRefP6VECTOR);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetMatrix__4CRefP7MATRIX4);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetClq__4CRefP3CLQ);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetLm__4CRefP2LM);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetSmp__4CRefP3SMP);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetBasic__4CRefP5BASIC);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", SetMethod__4CRefP7CMethod);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", RefCoerceS32__4CRef);
+
+INCLUDE_ASM(const s32, "P2/splice/ref", RefCoerceF32__4CRef);
