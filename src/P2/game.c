0c01a0c52bd970d2c7d6fa3eba3b1c380e0ddbbb
Lilly <flowerlilly648@gmail.com>
2024-09-09 01:54:04 -0400

    Matched a few functions in game.c and updated the symbol addressess

--- src/P2/game.c
+++ src/P2/game.c
@@ -1,6 +1,12 @@
 #include <game.h>
 
-INCLUDE_ASM(const s32, "P2/game", StartupGame__Fv);
+
+void StartupGame(void)
+{
+  StartGame();
+  return;
+}
+
 INCLUDE_ASM(const s32, "P2/game", func_00160090);
 
 INCLUDE_ASM(const s32, "P2/game", search_level_by_load_data);
@@ -8,11 +14,13 @@ INCLUDE_ASM(const s32, "P2/game", search_level_by_load_data);
 INCLUDE_ASM(const s32, "P2/game", search_level_by_id);
 
 INCLUDE_ASM(const s32, "P2/game", PchzFriendlyFromWid);
+
 INCLUDE_ASM(const s32, "P2/game", func_00160178);
 
 INCLUDE_ASM(const s32, "P2/game", call_search_level_by_id);
 
 INCLUDE_ASM(const s32, "P2/game", FFindLevel);
+
 INCLUDE_ASM(const s32, "P2/game", func_001601D0);
 
 INCLUDE_ASM(const s32, "P2/game", get_level_completion_by_id);
@@ -51,20 +59,36 @@ INCLUDE_ASM(const s32, "P2/game", FUN_00160948);
 
 INCLUDE_ASM(const s32, "P2/game", calculate_percent_completion);
 
-INCLUDE_ASM(const s32, "P2/game", SetCcharm__Fi);
+void SetCcharm(int nParam)
+{
+  g_pgsCur->ccharm = nParam;
+  return;
+}
 
 INCLUDE_ASM(const s32, "P2/game", FCharmAvailable__Fv);
+
 INCLUDE_ASM(const s32, "P2/game", func_00160C90);
 
 INCLUDE_ASM(const s32, "P2/game", PfLookupDialog__FP2LS3OID);
 
 INCLUDE_ASM(const s32, "P2/game", clr_8_bytes_1);
 
-INCLUDE_ASM(const s32, "P2/game", FUN_00160ce8);
+void FUN_00160ce8(int param_1)
+{
+  *(undefined4 *)(param_1 + 4) = 0;
+  return;
+}
 
-INCLUDE_ASM(const s32, "P2/game", OnGameAlarmTriggered);
+void OnGameAlarmTriggered(GAME *pgame)
+{
+  pgame->cAlarmsTriggered++;
+}
 
-INCLUDE_ASM(const s32, "P2/game", OnGameAlarmDisabled);
+void OnGameAlarmDisabled(GAME *pgame)
+{
+  pgame->cAlarmsTriggered--;
+  return;
+}
 
 INCLUDE_ASM(const s32, "P2/game", grfvault_something__Fv);
 
