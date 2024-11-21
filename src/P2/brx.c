bd89d36d65a005223381e46b6027c100173406df
Zac <theonlyzac@gmail.com>
2024-11-21 19:14:20 +0000

    Split brx.c

---
+++ src/P2/brx.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM(const s32, "P2/brx", StartupBrx__Fv);
+
+INCLUDE_ASM(const s32, "P2/brx", PloNew__F3CIDP2SWP3ALO3OIDi);
+
+INCLUDE_ASM(const s32, "P2/brx", LoadOptionFromBrx__FPvP5EOPIDP18CBinaryInputStream);
+
+INCLUDE_ASM(const s32, "P2/brx", snd_SendIOPCommandAndWait);
+
+INCLUDE_ASM(const s32, "P2/brx", snd_SendIOPCommandNoWait);
+
+INCLUDE_ASM(const s32, "P2/brx", LoadOptionsFromBrx__FPvP18CBinaryInputStream);
+
+INCLUDE_ASM(const s32, "P2/brx", IploFromStockOid__Fi);
+
+INCLUDE_ASM(const s32, "P2/brx", LoadSwObjectsFromBrx__FP2SWP3ALOP18CBinaryInputStream);
+
+INCLUDE_ASM(const s32, "P2/brx", SetLoDefaults__FP2LO);
