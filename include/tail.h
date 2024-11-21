8fac44d0a965107266d04b35705dda94bab05ea3
Zac <theonlyzac@gmail.com>
2024-11-21 20:13:45 +0000

    Split tail.c

---
+++ include/tail.h
@@ -0,0 +1,17 @@
+/**
+ * @file tail.h
+ *
+ * @brief JT tail physics.
+ */
+#ifndef TAIL_H
+#define TAIL_H
+
+#include "common.h"
+
+// todo Implement struct.
+struct TAIL
+{
+    // ...
+};
+
+#endif // TAIL_H
