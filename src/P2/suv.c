b9ee04b69b58db7bba1c26e10adc388fb1f91820
Zac <theonlyzac@gmail.com>
2024-11-25 21:56:30 +0000

    Cleanup splits and split suv.c

---
+++ src/P2/suv.c
@@ -0,0 +1,76 @@
+#include "common.h"
+
+INCLUDE_ASM(const s32, "P2/suv", InitSuv__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", PostSuvLoad__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", PresetSuvAccel__FP3SUVf);
+
+INCLUDE_ASM(const s32, "P2/suv", GExcludeAlm__FiP2LMf);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvBalance__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", DsGetTrackRelative__Ffff);
+
+INCLUDE_ASM(const s32, "P2/suv", FIsSuvAheadOf__FP3SUVT0);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvLine__FP3SUVPi);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvHeading__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvWheels__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvExpls__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", BoostSuv__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", FUN_001d9338);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvVolumes__FP3SUVi);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvSounds__FP3SUVf);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvPuncher__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuv__FP3SUVf);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvActive__FP3SUVP3JOYf);
+
+INCLUDE_ASM(const s32, "P2/suv", FUN_001da170);
+INCLUDE_ASM(const s32, "P2/suv", func_001DA310);
+
+INCLUDE_ASM(const s32, "P2/suv", AddSuvCustomXps__FP3SUVP2SOiP3BSPT3PP2XP);
+INCLUDE_ASM(const s32, "P2/suv", func_001DA6E0);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvInfluences__FP3SUVP2RO);
+
+INCLUDE_ASM(const s32, "P2/suv", RenderSuvSelf__FP3SUVP2CMP2RO);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvBounds__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", CollectSuvPrize__FP3SUV3PCKP3ALO);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvShapes__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", UpdateSuvXfWorld__FP3SUV);
+
+INCLUDE_ASM(const s32, "P2/suv", GetSuvCpdefi__FP3SUVfP6CPDEFI);
+
+INCLUDE_ASM(const s32, "P2/suv", OnSuvActive__FP3SUViP2PO);
+
+INCLUDE_ASM(const s32, "P2/suv", HandleSuvMessage__FP3SUV5MSGIDPv);
+
+INCLUDE_ASM(const s32, "P2/suv", SetSuvTrack__FP3SUVP5SHAPE);
+
+INCLUDE_ASM(const s32, "P2/suv", SetSuvLine__FP3SUVP5SHAPE);
+
+INCLUDE_ASM(const s32, "P2/suv", AddSuvCheckPoint__FP3SUVP3VOL);
+
+INCLUDE_ASM(const s32, "P2/suv", AddSuvFeature__FP3SUVP3VOLffP4EXPLT4);
+
+INCLUDE_ASM(const s32, "P2/suv", SetSuvSuvs__FP3SUV4SUVS);
+
+INCLUDE_ASM(const s32, "P2/suv", ResetSuv__FP3SUV);
+INCLUDE_ASM(const s32, "P2/suv", func_001DB448);
+
+INCLUDE_ASM(const s32, "P2/suv", VecCombo__FG8VU_FLOATG9VU_VECTORT0T1);
