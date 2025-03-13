efbb8ec2058d451a0f777bd8e84bc8f77df216e6
Zac <theonlyzac@gmail.com>
2025-03-13 00:32:29 +0000

    Match IgsAllocGsb

--- include/gs.h
+++ include/gs.h
@@ -16,7 +16,33 @@ struct GSB
 };
 
 /**
- * 
+ * @brief Initializes a GSB with the given values.
+ *
+ * @param pgsb The GSB to initialize.
+ * @param igsMin The minimum index.
+ * @param igsMax The maximum index.
+ */
+void InitGsb(GSB *pgsb, int igsMin, int igsMax);
+
+/**
+ * @brief Resets the igsMac to the minimum index.
+ *
+ * @param pGsb The GSB to reset.
+ */
+void ResetGsb(GSB *pGsb);
+
+/**
+ * @brief Allocates a range of indices in the GSB.
+ *
+ * @param pgsb The GSB to allocate from.
+ * @param iCount The number of indices to allocate.
+ *
+ * @return The old value of igsMac.
+ */
+int IgsAllocGsb(GSB *pgsb, int iCount);
+
+/**
+ *
 */
 void ClearFrameBuffers();
 
