0cc43177cfc19a04762e277b22aaa2a3b8a417e0
Zac <theonlyzac@gmail.com>
2025-06-18 21:19:40 +0000

    Edit signatures of strlen and strchr

--- src/P2/text.c
+++ src/P2/text.c
@@ -40,30 +40,23 @@ INCLUDE_ASM(const s32, "P2/text", func_001E20B0);
 INCLUDE_ASM(const s32, "P2/text", _snprintf);
 INCLUDE_ASM(const s32, "P2/text", func_001E20F8);
 
-INCLUDE_ASM(const s32, "P2/text", strlen);
-/**
- * @todo 100% matched, but it conflicts with built-in strlen.
- * https://decomp.me/scratch/cJZam
- */
-/*
-uint strlen(char *pchz)
+extern "C" uint strlen(const char *pchz)
 {
     uint len = 0;
-    
+
     while(*pchz != '\0') {
         pchz++;
         len++;
     }
-    
+
     return len;
 }
-*/
 
 INCLUDE_ASM(const s32, "P2/text", strcpy);
 
 INCLUDE_ASM(const s32, "P2/text", strcpy1);
 
-char *strchr(char *pchz, int ch)
+extern "C" char *strchr(char *pchz, int ch)
 {
     while(*pchz != '\0')
     {
