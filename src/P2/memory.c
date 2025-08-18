1a2a6acd3ff47d580e100c99e0d82054dd89f3a7
545u <47539026+545u@users.noreply.github.com>
2025-08-19 01:09:44 +0300

    Match most of P2/memory.c

--- src/P2/memory.c
+++ src/P2/memory.c
@@ -1,5 +1,13 @@
 #include <memory.h>
+#include <thread.h>
 #include <sce/memset.h>
+
+extern int g_swAllocBase;
+extern int g_swAllocPtr;
+extern int g_stackAllocPtr;
+extern CRITSECT g_stackAllocCritSect;
+extern int g_stackAllocDepth;
+extern int g_stackAllocStack[];
 
 INCLUDE_ASM(const s32, "P2/memory", PvAllocGlobalImpl__Fi);
 INCLUDE_ASM(const s32, "P2/memory", LAB_0018D4F0);
@@ -13,13 +21,34 @@ void HardLockError()
 }
 #endif
 
-void CheckForOutOfMemory() {
+void CheckForOutOfMemory()
+{
     return;
 }
 
+/**
+ * @todo 99.50% matching. Only a register mismatch.
+ * https://decomp.me/scratch/It1kw
+ */
 INCLUDE_ASM(const s32, "P2/memory", PvAllocSwImpl__Fi);
+#ifdef SKIP_ASM
+void *PvAllocSwImpl(int cb)
+{
+    if(cb == 0)
+    {
+        return nullptr;
+    }
+    
+    CheckForOutOfMemory();
+    g_swAllocPtr += (cb + 0x0f) & -0x10;
+    return (void *)g_swAllocPtr;
+}
+#endif
 
-INCLUDE_ASM(const s32, "P2/memory", FreeSw__Fv);
+void FreeSw()
+{
+    g_swAllocPtr = g_swAllocBase;
+}
 
 void *PvAllocSwCopyImpl(int cb, void *pvBase)
 {
@@ -43,9 +72,23 @@ void *PvAllocSwClearImpl(int cb)
     return pvBlock;
 }
 
-INCLUDE_ASM(const s32, "P2/memory", InitStackImpl__Fv);
+void InitStackImpl()
+{
+    EnterCritSect(&g_stackAllocCritSect);
+    g_stackAllocStack[++g_stackAllocDepth] = g_stackAllocPtr;
+}
 
-INCLUDE_ASM(const s32, "P2/memory", PvAllocStackImpl__Fi);
+void *PvAllocStackImpl(int cb)
+{
+    if(cb == 0)
+    {
+        return nullptr;
+    }
+    
+    CheckForOutOfMemory();
+    g_stackAllocPtr -= (cb + 0x0f) & -0x10;
+    return (void *)g_stackAllocPtr;
+}
 
 void *PvAllocStackClearImpl(int cb)
 {
@@ -58,7 +101,11 @@ void *PvAllocStackClearImpl(int cb)
     return pvBlock;
 }
 
-INCLUDE_ASM(const s32, "P2/memory", FreeStackImpl__Fv);
+void FreeStackImpl()
+{
+    g_stackAllocPtr = g_stackAllocStack[g_stackAllocDepth--];
+    LeaveCritSect(&g_stackAllocCritSect);
+}
 
 void *malloc(uint __size)
 {
@@ -70,7 +117,8 @@ void *_malloc_r(_reent *pre, uint __size)
     return (void *)0x0;
 }
 
-void free(void *pv) {
+void free(void *pv)
+{
     return;
 }
 
@@ -79,7 +127,8 @@ void *__builtin_new(unsigned int cb)
     return PvAllocSwClearImpl(cb);
 }
 
-void __builtin_delete() {
+void __builtin_delete()
+{
     return;
 }
 
