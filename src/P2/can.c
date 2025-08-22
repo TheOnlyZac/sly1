5698b18132b86d6bfd3cee8f4a7cd97e53b15e9f
545u <47539026+545u@users.noreply.github.com>
2025-08-23 02:38:55 +0300

    Rename P2/can to P2/cat and match StartupCatalog

--- src/P2/can.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM(const s32, "P2/can", FUN_00140790__Fv);
-
-INCLUDE_ASM(const s32, "P2/can", FUN_001407A0);
---
+++ src/P2/cat.c
@@ -0,0 +1,10 @@
+#include "cd.h"
+
+extern CDS g_cds;
+
+void StartupCatalog()
+{
+    g_cds = CDS_Ready;
+}
+
+INCLUDE_ASM(const s32, "P2/cat", FUN_001407A0);
