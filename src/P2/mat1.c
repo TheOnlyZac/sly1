9211a32bfe9ac4aef8c29cef09a86c6e9e118088
545u <47539026+545u@users.noreply.github.com>
2025-08-25 02:43:23 +0300

    Split P2/mat into different files

---
+++ src/P2/mat1.c
@@ -0,0 +1,24 @@
+/**
+ * @note Should be merged with mat2.c and mat_AddMatrix4Matrix4.
+ */
+#include <mat.h>
+
+INCLUDE_ASM(const s32, "P2/mat1", PostCopyMatrix3__7MATRIX4);
+
+INCLUDE_ASM(const s32, "P2/mat1", __as__7MATRIX4RC7MATRIX3);
+
+INCLUDE_ASM(const s32, "P2/mat1", MatMulMatTransMat__FRC7MATRIX3T0);
+
+INCLUDE_ASM(const s32, "P2/mat1", LoadIdentityMatrix4__FP7MATRIX4);
+
+INCLUDE_ASM(const s32, "P2/mat1", __ml__FRC7MATRIX4G8VU_FLOAT);
+
+INCLUDE_ASM(const s32, "P2/mat1", __ml__FRC7MATRIX4T0);
+
+INCLUDE_ASM(const s32, "P2/mat1", junk_001886F8);
+
+INCLUDE_ASM(const s32, "P2/mat1", MultiplyMatrix4Vector__FP7MATRIX4P6VECTORfT1);
+
+INCLUDE_ASM(const s32, "P2/mat1", __ml__FRC7MATRIX4G10VU_VECTOR4);
+
+INCLUDE_ASM(const s32, "P2/mat1", TransposeMatrix4__FP7MATRIX4T0);
