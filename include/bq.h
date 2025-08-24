55a9b436c2fe755039c1d87299045acb9fd7d2f7
545u <47539026+545u@users.noreply.github.com>
2025-08-25 01:22:59 +0300

    Split P2/bq and match a few functions

--- include/bq.h
+++ include/bq.h
@@ -7,12 +7,48 @@
 #define BQ_H
 
 #include "common.h"
-#include <util.h>
 
 /**
- * @brief Byte Queue
+ * @brief TODO.
+ */
+class CQueueInput
+{
+public:
+    virtual int CbRead(int cb, void *pv);
+};
+
+/**
+ * @brief TODO.
+ */
+class CQueueOutput
+{
+    // ...
+};
+
+/**
+ * @brief TODO.
+ */
+class CQueueInputMemory : public CQueueInput
+{
+public:
+    int CbRead(int cb, void *pv);
+};
+
+/**
+ * @brief TODO.
+ */
+class CQueueInputFile : public CQueueInput
+{
+public:
+    int CbRead(int cb, void *pv);
+};
+
+/**
+ * @brief Byte Queue.
 */
-class CByteQueue {
+class CByteQueue
+{
+public:
     byte *m_ab;
     int m_cb;
     int m_ibFree;
@@ -22,7 +58,49 @@ class CByteQueue {
     int m_cbDrain;
     int m_cbTotal;
 
-    // todo: class methods
+    /**
+     * @brief Initialize the byte queue.
+     *
+     * @param cb Number of bytes.
+     * @param pv Pointer to the data.
+     */
+    void Init(int cb, void *pv);
+
+    /**
+     * @brief Reset the byte queue.
+     */
+    void Reset();
+
+    /**
+     * @brief TODO.
+     *
+     * @param cb Number of bytes.
+     * @param pqi Pointer to the input.
+     */
+    int CbFill(int cb, CQueueInput *pqi);
+
+    /**
+     * @brief TODO.
+     *
+     * @param cb Number of bytes.
+     * @param pqo Pointer to the output.
+     */
+    int CbDrain(int cb, CQueueOutput *pqo);
+
+    /**
+     * @brief TODO.
+     *
+     * @param cb Number of bytes.
+     * @param pb Pointer to the data.
+     */
+    int CbFill(int cb, byte *pb);
+
+    /**
+     * @brief Drain bytes from the byte queue.
+     *
+     * @param cb Number of bytes to drain.
+     */
+    void FreeDrain(int cb);
 };
 
 #endif // BQ_H
