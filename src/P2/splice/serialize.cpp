53e8a1029c69ce2434a7ac0bad90bbfda92d6c40
Nicolas Nytko <nicknytko@gmail.com>
2026-09-20 16:10:32 -0500

    add function scaffolding

--- src/P2/splice/serialize.cpp
+++ src/P2/splice/serialize.cpp
@@ -1,3 +1,10 @@
 #include <splice/serialize.h>
+#include <splice/vecmat.h>
 
 INCLUDE_ASM("asm/nonmatchings/P2/splice/serialize", PpairSerializeIn__FP18CBinaryInputStream);
+#ifdef SKIP_ASM
+CPair *PpairSerializeIn(CBinaryInputStream *pstrm)
+{
+
+}
+#endif
